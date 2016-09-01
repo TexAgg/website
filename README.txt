4702d0c0b8fe255d4569e685f393c5c66caa7e4a
Matt Gaikema <mgaikema1@gmail.com>
2016-09-01 00:35:49 -0500

    Nest projects

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
 ﻿Rewriting my website (again) using ASP.NET.
+This is more for fun and probably will not be published.
--- website/Models/Project.cs
+++ website/Models/Project.cs
@@ -1,19 +1,24 @@
-﻿using System;
+﻿/*
+ * This class provides an object for when the JSON file
+ * containing a list of my projects is deserialized.
+ */
+
+using System;
 using System.Collections.Generic;
 
 namespace website.Models
 {
-	public class Item
-	{
-		public string about { get; set; }
-		public string display { get; set; }
-		public string id { get; set; }
-		public string name { get; set; }
-	}
-
-	public class RootObject
+	public class Projects
 	{
 		public List<Item> item { get; set; }
+
+		public class Item
+		{
+			public string about { get; set; }
+			public string display { get; set; }
+			public string id { get; set; }
+			public string name { get; set; }
+		}
 	}
 }
 
