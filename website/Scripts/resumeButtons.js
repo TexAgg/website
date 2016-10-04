89bfe14fd15edb440e7a367c1b0f586831bb181e
Matt Gaikema <mgaikema1@gmail.com>
2016-10-03 19:57:16 -0500

    Switch between resume and cv

---
+++ website/Scripts/resumeButtons.js
@@ -0,0 +1,17 @@
+﻿$("[name='my-checkbox']").bootstrapSwitch();
+// http://www.bootstrap-switch.org/events.html
+$('input[name="my-checkbox"]').on('switchChange.bootstrapSwitch', function(event, state) {
+  console.log(state); // true | false
+	var img = document.getElementById("image");
+	if (state) {
+		img.src = "~/Content/Resume/resume.png";
+	}
+	else {
+		img.src = "~/Content/Resume/cv.png";
+	}
+});
+
+// http://stackoverflow.com/a/8784019/5415895
+function changeImage() {
+	var img = document.getElementById("image");
+}
