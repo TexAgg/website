2d1abe77359c364d413217d3d9a2cff7fca7e8f1
Matt Gaikema <mgaikema1@gmail.com>
2016-10-14 13:13:37 -0500

    Add make active class for navbar

---
+++ website/Utilities/HTMLUtilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Utilities
+{
+	/// <summary>
+	/// HTML utilities.
+	/// </summary>
+	public static class HTMLUtilities
+	{
+		/// <summary>
+		/// An extension method to make a navbar link class active.
+		/// Code found here: http://stackoverflow.com/a/22408778/5415895
+		/// </summary>
+		/// <returns>Either active or null.</returns>
+		/// <param name="urlHelper">URL helper.</param>
+		/// <param name="controller">Controller.</param>
+		public static string MakeActiveClass(this UrlHelper urlHelper, string controller)
+		{
+			string result = "active";
+
+			string controllerName = urlHelper.RequestContext.RouteData.Values["controller"].ToString();
+
+			if (!controllerName.Equals(controller, StringComparison.OrdinalIgnoreCase))
+			{
+				result = null;
+			}
+
+			return result;
+		}
+	}
+}
+
