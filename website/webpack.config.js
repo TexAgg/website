95f75192294acffd0cf9a3accb1c74fb6bfa3bde
Matt Gaikema <mgaikema1@gmail.com>
2016-10-24 11:20:02 -0500

    resume bundle

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -2,23 +2,28 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  context: __dirname + "/Scripts",
-  entry: "./main.js",
-  output: {
-    path: __dirname + "/dist",
-    filename: "bundle.min.js"
-  },
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
-  ],
-  module: {
-    loaders: [{ 
-        test: /\.css$/,                 
-        //include: [path.resolve(__dirname, "not_exist_path")],
-        loader: "style-loader!css-loader" 
-      }
-    ]
-  }
+	context: __dirname + "/Scripts",
+	entry: {
+		// Main bundle
+		main: "./main.js", 
+		// Resume bundle
+		resume: "./resumeButtons.js"
+	},
+	output: {
+		path: __dirname + "/dist",
+		filename: "[name].min.js"
+	},
+	plugins: [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+	],
+	module: {
+		loaders: [{ 
+				test: /\.css$/,                 
+				//include: [path.resolve(__dirname, "not_exist_path")],
+				loader: "style-loader!css-loader" 
+			}
+		]
+	}
 };
