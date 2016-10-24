65379d32a9d287f417d094d512bb6c0611ef6f9d
Matt Gaikema <mgaikema1@gmail.com>
2016-10-23 22:02:18 -0500

    webpack

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -6,12 +6,17 @@ module.exports = {
   context: __dirname + "/Scripts",
   entry: "./main.js",
   output: {
-    path: __dirname + "/Scripts",
+    path: __dirname + "/dist",
     filename: "bundle.min.js"
   },
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new CopyWebpackPlugin([
+      // Copy clippy.js assets to dist folder.
+      {from: __dirname + "/node_modules/clippy.js/build/clippy.min.js", to: __dirname + "/dist/clippy.js/clippy.min.js"},
+      {from: __dirname + "/node_modules/clippy.js/build/clippy.css", to: __dirname + "/dist/clippy.js/clippy.css"}
+    ])
   ]
 };
