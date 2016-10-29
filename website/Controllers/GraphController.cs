088524094d0342f7f090e4407080a35a42004ca7
Matt Gaikema <mgaikema1@gmail.com>
2016-10-29 12:59:00 -0500

    Graph page

---
+++ website/Controllers/GraphController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace website.Controllers
+{
+    public class GraphController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Graph()
+		{
+			return File("Content/Graph/graph.gv", "text/plain");
+		}
+
+		public ActionResult Source()
+		{
+			return File("Content/Graph/graph.wl", "text/plain");
+		}
+    }
+}
