5a08d8adbd263f142c4e4102213b5dbc3bff4e48
Matt Gaikema <mgaikema1@gmail.com>
2016-10-29 13:01:11 -0500

    comments

--- website/Controllers/GraphController.cs
+++ website/Controllers/GraphController.cs
@@ -6,18 +6,30 @@ using System.Web.Mvc;
 
 namespace website.Controllers
 {
-    public class GraphController : Controller
+    /// <summary>
+    /// Graph controller.
+    /// </summary>
+	public class GraphController : Controller
     {
-        public ActionResult Index()
+        /// <summary>
+        /// Display information about the graph.
+        /// </summary>
+		public ActionResult Index()
         {
             return View();
         }
 
+		/// <summary>
+		/// The dot file.
+		/// </summary>
 		public ActionResult Graph()
 		{
 			return File("Content/Graph/graph.gv", "text/plain");
 		}
 
+		/// <summary>
+		/// The source code for making the graph.
+		/// </summary>
 		public ActionResult Source()
 		{
 			return File("Content/Graph/graph.wl", "text/plain");
