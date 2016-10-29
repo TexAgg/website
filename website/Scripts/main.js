9e9529938c5ab6114151a62778ed59410753f2aa
Matt Gaikema <mgaikema1@gmail.com>
2016-10-29 15:23:15 -0500

    utils

--- website/Scripts/main.js
+++ website/Scripts/main.js
@@ -8,41 +8,14 @@
 */
 
 require("../node_modules/clippy.js/build/clippy.css");
+// Load this globally.
 // https://medium.com/@dtothefp/why-can-t-anyone-write-a-simple-webpack-tutorial-d0b075db35ed#.uk6oyw973
 require("script!../node_modules/clippy.js/build/clippy.min.js");
+var utils = require('./modules/utils');
+
+//utils.httpGetAsync("http://stackoverflow.com/questions/247483/http-get-request-in-javascript", console.log);
 
 clippy.load('Clippy', function(agent) {
 	agent.show();
 	agent.speak("Hello! Welcome to the website of Matt Gaikema!");
-
-	/*
-		Uncomment to add mousenter events to navbar.
-	*/
-	/*
-	document.getElementById('index-nav-li').onmouseenter = function() {
-		agent.stop();
-		agent.speak("Click here to read about me!");
-	}
-
-	document.getElementById('projects-nav-li').onmouseenter = function() {
-		agent.stop();
-		agent.speak("Click here to see some projects I have worked on!");
-	}
-
-	document.getElementById('skills-nav-li').onmouseenter = function() {
-		agent.stop();
-		agent.speak("Click here to see my skills!");
-	}
-
-	document.getElementById('resume-nav-li').onmouseenter = function() {
-		agent.stop();
-		agent.speak("Click here to see my resume!");
-	}
-
-	document.getElementById('contact-nav-li').onmouseenter = function() {
-		agent.stop();
-		agent.speak('Click here to see my contact information!');
-	}
-	*/
-
 });
