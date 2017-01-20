0648ebefcf693106d7fd1c55b670a2187760c723
Matt Gaikema <mgaikema1@gmail.com>
2017-01-20 15:13:44 -0600

    modify build script. Now can handle removing node modules for release and installing them back when neaded

--- website/Models/AsciiImage.cs
+++ website/Models/AsciiImage.cs
@@ -4,11 +4,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace website.Models
 {
+	/// <summary>
+	/// ASCII image.
+	/// </summary>
 	public class AsciiImage
 	{
+		/// <summary>
+		/// Gets or sets the file.
+		/// </summary>
+		/// <value>The file.</value>
 		[Required]
 		public HttpPostedFileBase File { get; set; }
+		/// <summary>
+		/// Gets or sets the width.
+		/// </summary>
+		/// <value>The width.</value>
 		public int? Width { get; set; }
+		/// <summary>
+		/// Gets or sets the height.
+		/// </summary>
+		/// <value>The height.</value>
 		public int? Height { get; set; }
 	}
 }
