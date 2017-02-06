57a1b6952e2cd778408bac2b25b29851cbff5aae
Matt Gaikema <mgaikema1@gmail.com>
2017-02-05 21:13:25 -0600

    begin attom stuff

---
+++ website/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace website.Controllers
+{
+    public class BlogController : Controller
+    {
+		// http://stackoverflow.com/a/5103589/5415895
+		Atom10FeedFormatter _feed;
+
+		public BlogController()
+		{
+			_feed = new Atom10FeedFormatter();
+			using (XmlReader reader = XmlReader.Create("http://mattgaikema.blogspot.com/feeds/posts/default"))
+			{
+				_feed.ReadFrom(reader);
+			}
+		}
+
+		public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
